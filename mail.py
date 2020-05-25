badc169a87fb2e1a005dc4f1f7a92b819f77b2e9
Vishal Yadav <vishalyadav831874@gmail.com>
2020-05-25 14:22:25 +0530

    last one final commit

---
+++ mail.py
@@ -0,0 +1,39 @@
+import struct
+import numpy as np
+from keras.models import load_model
+from keras.utils import np_utils
+def read_idx(filename):
+        """Credit: https://gist.github.com/tylerneylon"""
+        with open(filename, 'rb') as f:
+            zero, data_type, dims = struct.unpack('>HBB', f.read(4))
+            shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+            return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
+x_train = read_idx("./fashion_mnist/train-images-idx3-ubyte")
+y_train = read_idx("./fashion_mnist/train-labels-idx1-ubyte")
+x_test = read_idx("./fashion_mnist/t10k-images-idx3-ubyte")
+y_test = read_idx("./fashion_mnist/t10k-labels-idx1-ubyte")  
+img_rows = x_train[0].shape[0]
+img_cols = x_train[1].shape[0]
+
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+x_train /= 255
+x_test /= 255
+
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_test.shape[1]
+num_pixels = x_train.shape[1] * x_train.shape[2]
+
+model=load_model("result.txt")
+score = model.evaluate(x_test, y_test, verbose=0)
+if score[1] < 0.95:
+         print("Model is creating, it is busy in finding perfect weights")
+else:
+        print("Model is ready")
