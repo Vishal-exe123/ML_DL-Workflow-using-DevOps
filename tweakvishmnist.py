2fe18185ca1d7842d9c4b9d33875f955e30031bf
Vishal Yadav <vishalyadav831874@gmail.com>
2020-05-25 23:14:43 +0530

    now this is the final trigger.

--- tweakvishmnist.py
+++ tweakvishmnist.py
@@ -140,7 +140,7 @@ def vishloaddata():
             print('Test loss:', score[0])
             print('Test accuracy:', score[1])
             t+=1
-            if score[1] < 0.95 or i<4:
+            if score[1] < 0.95 or (i == 4):
                 print("Model is creating with analysis of what  will be the best  accuracy")
                 continue
             else:
